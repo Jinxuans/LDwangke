e233cccd1c75f9df26e4d1f332ef76e02a48862a
锦轩 <10372102+jinxuan6@user.noreply.gitee.com>
2026-03-17 00:38:11 +0800

    refactor trim legacy service hosts

--- go-api/internal/service/compat_hosts.go
+++
@@ -1,9 +0,0 @@
-package service
-
-// Compat receiver hosts that remain only because sibling files still attach
-// methods to them during the compatibility-host phase.
-type AuthService struct{}
-
-type UserCenterService struct{}
-
-var userCenterService = &UserCenterService{}
